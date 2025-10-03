2f67792c29a0e9fceba0577d2858251474b17662
Jeremy Southern <jezzd11@outlook.com>
2025-10-03 12:51:47 +0100

    Update sketch.js

--- exercise2-3/sketch.js
+++ exercise2-3/sketch.js
@@ -1,30 +1,38 @@
-let x = 25;
-let y =25;
-let moveP;
-let moveM;
-let moveAmount = 10;
+let x, y;          
+let xSpeed = 3;     
+let ySpeed = 3;     
+let diameter = 100; 
+let radius = diameter / 2;
 
 function setup()
 {
-    createCanvas(600,400);
-
+  createCanvas(600, 400);
+  x = radius; 
+  y = radius;
 }
-function draw()
+
+function draw() 
 {
-    background(0);
-    circle(x, y, 100);
-    if (x <= 550 && y <= 350)
-    {
-        
-    }
-    if (x >= 550 && y >= 350)
-    {
-    
-    }
-   
-    
-    
-    
+  background(240);
+
+  
+  fill(0, 150, 255);
+  noStroke();
+  ellipse(x, y, diameter, diameter);
+
+  
+  x += xSpeed;
+  y += ySpeed;
+
+  
+  if (x + radius > width || x - radius < 0) 
+  {
+    xSpeed *= -1;
+  }
+
+  
+  if (y + radius > height || y - radius < 0) 
+  {
+    ySpeed *= -1;
+  }
 }
-
-
